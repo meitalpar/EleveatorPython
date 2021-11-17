7c5a93a73bbcbcd045e62a7d40229810ca15f3f0
meitalpar <93653029+meitalpar@users.noreply.github.com>
2021-11-17 13:36:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -60,47 +60,22 @@ def well (b:building,src:int,des:int):
 
 
 
-def del3():
-    with open(output) as myFile:
-        lines = myFile.readlines()
-        if len(lines) > 0:
-            last_line = lines[len(lines) - 1]
-            lines[len(lines) - 1] = last_line.rstrip()
-        with open(output, 'w') as myFile:
-            myFile.writelines(lines)
-
-
-
 
 
 with open(callsname) as in_file:
-#in_file = open(callsname, "rt")
     reader = csv.reader(in_file)
+    with open(output,'w', newline='') as out_file:
 
-    #out_file = open(output, "wt")
-    #out_file = open(output, "w")
-#    writer = csv.writer(out_file)
-    list_for_csv = []
+        writer = csv.writer(out_file)
 
-    for row in reader:
-        #del3()
-        src = int(row[2])
-        des = int(row[3])
-        row[5] = (well(b, src, des))
-        list_for_csv.append(str(row))
-    print(list_for_csv)
+        for row in reader:
+            src = int(row[2])
+            des = int(row[3])
+            row[5] = (well(b, src, des))
+            writer.writerow(row)
 
-        #writer.writerow(row[0])
-#a_list = ["abc", "def", "ghi"]
-textfile = open(output, "w")
-for element in list_for_csv:
-        textfile.write(element + "\n")
-    #.write(element + "\n")
-#textfile.close()
-    #writer.w
 in_file.close()
-textfile.close()
-#out_file.close()
+out_file.close()
 
 
 
